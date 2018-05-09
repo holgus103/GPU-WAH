4ce3748745df731eb962ffa753925cba79ffe3cd
holgus103 <kubasuchan@hotmail.com>
2018-05-09 02:31:05 +0200

    Fixed most of IntelliSense issues

--- kernels.cu
+++ kernels.cu
@@ -1,5 +1,7 @@
 #include <cuda.h>
+#include <cuda_runtime.h>
 #include <cuda_device_runtime_api.h>
+#include "device_launch_parameters.h"
 #include "kernels.h"
 #include "tests.h"
 #include <stdio.h>
